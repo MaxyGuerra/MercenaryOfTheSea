107e9c7c3986af0934d99eb8f0daa7c2e7b6d31a
Autumn <66489920+autumnsbeast@users.noreply.github.com>
2020-09-24 15:54:09 -0300

    Respaldo(Intento de que el enemigo "muerto" siga al player)

---
+++ Assets/OurScripts/BossAIScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public enum BossState { IDLE,FOLLOW}
+public class BossAIScript : MonoBehaviour
+{
+
+    NavMeshAgent navAgent;
+    public float remainingDistance;
+    public bool isFollowingPlayer = false;
+
+    public int enemyHealth = 3;
+    public Transform playerTarget;
+
+
+
+    private void Awake()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        remainingDistance = Mathf.Infinity;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("I'm following you");
+
+            isFollowingPlayer = true;
+        }
+    }
+
+    void FollowPlayer()
+    {
+        if (playerTarget ==null) return;
+
+        if (!navAgent.SetDestination(playerTarget.position)) return;
+
+        if (!navAgent.hasPath) return;
+        remainingDistance = navAgent.remainingDistance;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFollowingPlayer == true)
+        {
+            FollowPlayer();
+        }
+    }
+}
