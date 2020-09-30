ca4b4de1b4aa83f3f8d79c592e66c470d39d5ab4
MaxyGuerra <maxyguerrar@gmail.com>
2020-09-30 20:24:36 -0300

    Colliders puestos en el tentaculo

--- Assets/OurScripts/PlayerController.cs
+++ Assets/OurScripts/PlayerController.cs
@@ -62,6 +62,14 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Tentaculo"))
+        {
+            playerHealth--;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("EnemyBullet"))
