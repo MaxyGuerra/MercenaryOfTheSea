a62a8e25125e6e970ba6326df618c9f240b923de
Autumn <66489920+autumnsbeast@users.noreply.github.com>
2020-10-03 21:23:03 -0300

    Esto se borrará

---
+++ Assets/OurScripts/BossTextTutorial.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BossTextTutorial : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    public float skipTime = 1f;
+    bool bCanSkip;
+
+    void Start()
+    {
+
+    }
+    IEnumerator Type()
+    {
+        StartCoroutine(BlockButton());
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+
+        }
+    }
+
+    IEnumerator BlockButton()
+    {
+        bCanSkip = false;
+        yield return new WaitForSeconds(skipTime);
+        bCanSkip = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+      
+        if (other.gameObject.CompareTag("Boss"))
+        {
+            StartCoroutine(Type());
+        }
+    }
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E) && bCanSkip)///bCanSkip es el freno
+        {
+            NextSentence();
+        }
+
+    }
+
+    public void NextSentence()
+    {
+
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+        }
+
+
+    }
+}
--- Assets/OurScripts/PlayerController.cs
+++ Assets/OurScripts/PlayerController.cs
@@ -73,7 +73,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Tentaculo"))
+        if (collision.gameObject.CompareTag("Tentaculos"))
         {
             playerHealth--;
         }
