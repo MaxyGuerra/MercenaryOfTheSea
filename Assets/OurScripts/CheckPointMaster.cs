49d9855325bb72dc2bac78ebdac2991b6cf2faa8
Autumn <66489920+autumnsbeast@users.noreply.github.com>
2020-10-29 15:31:08 -0300

    Haciendo el Script de Checkpoint

---
+++ Assets/OurScripts/CheckPointMaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointMaster : MonoBehaviour
+{
+    private CheckPointMaster instance;
+    public Vector3 lastChackPointPos;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/OurScripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private CheckPointMaster gm;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<CheckPointMaster>();
+    }
+
+   void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gm.lastChackPointPos = transform.position;
+        }
+    }
+}
