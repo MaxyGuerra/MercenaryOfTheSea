9e3db15325c019b4af60e2a0a755d94a3eae0cbe
MaxyGuerra <66489175+MaxyGuerra@users.noreply.github.com>
2020-10-31 13:46:06 -0300

    vela + boya(wip)

---
+++ Assets/OurScripts/AreaBoya.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class AreaBoya : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
