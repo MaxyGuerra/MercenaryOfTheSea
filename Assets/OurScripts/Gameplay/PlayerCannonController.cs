791c25b33e907f3a99bb6f28f33fc97811b1b6fe
Autumn <66489920+autumnsbeast@users.noreply.github.com>
2020-10-31 17:50:55 -0300

    Raycast que detecta la dirección del disparo, listo

---
+++ Assets/OurScripts/Gameplay/PlayerCannonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCannonController : MonoBehaviour
+{
+    Transform playerTransform;
+    public Transform firePoint;
+    public bool canShoot = false;
+    public BulletController cannonBall;
+    public float cannonSpeed = 3;
+
+    public float shootingPower;
+
+    public float cadence = 3;
+    private float counter;
+
+    [Header("Rotation Settings")]
+    public float rotationSpeed = 3;
+    public Camera mainCamara;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray cameraRay = mainCamara.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLeght;
+
+        if(groundPlane.Raycast(cameraRay,out rayLeght))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLeght);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+        }
+    }
+}
---
+++ Assets/OurScripts/UI&Functionality/PlayerPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPosition : MonoBehaviour
+{
+    private CheckPointMaster gm;
+    private PlayerController playerReference;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<CheckPointMaster>();
+        transform.position = gm.lastChackPointPos;
+    }
+
+
+}
