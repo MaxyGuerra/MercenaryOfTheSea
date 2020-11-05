cf360ee5ba8b4235e93bbfc79ab326cc04656f1c
MaxyGuerra <66489175+MaxyGuerra@users.noreply.github.com>
2020-11-04 22:18:18 -0300

    Revert "Dispara en 360 pero no apunta bien y tampoco carga el tiro"
    This reverts commit 1eed96835ca1272579d744e0a7184aa0f728538c.

--- Assets/OurScripts/Gameplay/PlayerCannonController.cs
+++ Assets/OurScripts/Gameplay/PlayerCannonController.cs
@@ -8,23 +8,20 @@ public class PlayerCannonController : MonoBehaviour
     public Transform firePoint;
     public bool canShoot = false;
     public Rigidbody cannonBallBullet;
-    public float shootDistance = 100f;
-
+    
 
 
     public float cadence = 3;
     private float counter;
 
-
-    [Header("Cursor Settings")]
-
+    
+    //[Header("")]
+    
     private Camera mainCamara;
-    public GameObject cursor;
+    public GameObject cursor; 
     public LayerMask layer;
-    RaycastHit hit;
-
-
-
+    
+   
 
     // Start is called before the first frame update
     void Start()
@@ -32,27 +29,12 @@ public class PlayerCannonController : MonoBehaviour
         mainCamara = Camera.main;
     }
 
-    void FiringPoint()
+    void ShootCannonBall()
     {
-        Ray camRay = mainCamara.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(camRay, out hit, shootDistance, layer))
-        {
-            cursor.transform.position = hit.point + Vector3.up * 1f;
-
-            Vector3 Vo = CalculateVelocity(hit.point, transform.position, 1f);
-
-        }
+        
 
     }
 
-    void ShootCannon()
-    {
-        Rigidbody bullet = Instantiate(cannonBallBullet, firePoint.position, Quaternion.identity);
-        bullet.velocity = CalculateVelocity(hit.point, transform.position, 1f);
-    }
-
- 
     Vector3 CalculateVelocity(Vector3 target, Vector3 origin, float timeInAir)
     {
         Vector3 distance = target - origin;
@@ -64,13 +46,12 @@ public class PlayerCannonController : MonoBehaviour
         float heightY = distance.y;
         float heightXZ = distanceXZ.magnitude;
 
-        // Velocity X
-
         float Vxz = heightXZ / timeInAir;
         float Vy = heightXZ / timeInAir + 0.5f * Mathf.Abs(Physics.gravity.y) * timeInAir;
 
-        //Velocity Y
-        Vector3 result = distanceXZ.normalized * Vxz;
+
+        Vector3 result = distanceXZ.normalized;
+        result *= Vxz;
         result.y = Vy; 
 
         return result;
@@ -80,30 +61,30 @@ public class PlayerCannonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*Ray cameraRay = mainCamara.ScreenPointToRay(Input.mousePosition);
+        Ray cameraRay = mainCamara.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
         float rayLeght;
 
-         if(groundPlane.Raycast(cameraRay,out rayLeght))
-         {
+        if(groundPlane.Raycast(cameraRay,out rayLeght))
+        {
             Vector3 pointToLook = cameraRay.GetPoint(rayLeght);
             Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
-         }*/
-
+        }
 
         counter -= Time.deltaTime;
-        
-        FiringPoint();
 
         if (counter <= 0)
         {
             // CannonShoot
 
-            if (canShoot == true)
+            if (canShoot)
             {
                 counter = cadence;
-                ShootCannon();
-               
+
+                {
+                    ShootCannonBall();
+                }
+
             }
 
             else
