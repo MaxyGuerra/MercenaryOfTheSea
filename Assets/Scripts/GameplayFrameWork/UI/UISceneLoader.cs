24ee3dc45e0e4dfb1ba0222cf75bdc72080e1b1e
MaxyGuerra <66489175+MaxyGuerra@users.noreply.github.com>
2020-12-17 13:36:20 -0300

    profe cosas

---
+++ Assets/Scripts/GameplayFrameWork/UI/UISceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+ 
+    public class UISceneLoader : MonoBehaviour
+    {
+
+       
+        public int sceneIndex;
+
+        
+     
+        // Start is called before the first frame update
+        public void GameManagerLoadScene()
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+ 
+
