fa32a7a354b434197354db8983ff0502ce4bae63
Autumn <66489920+autumnsbeast@users.noreply.github.com>
2020-12-17 01:35:23 -0300

    Puliendo el win del tutorial

---
+++ Assets/OurScripts2/UI&Functionality/OneBossWinCondition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneBossWinCondition : MonoBehaviour
+{
+    public CanvasGroup winCanvas;
+
+    public GameObject addonsIcons;
+
+    public NewPlayerController playerReference;
+
+    private AudioSource _audioSource;
+    public AudioClip soundEfect;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Boss"))
+        {
+            _audioSource.PlayOneShot(soundEfect, 0.7F);
+
+            addonsIcons.gameObject.SetActive(false);
+
+            Destroy(other.gameObject);
+
+            playerReference.canMove = false;
+
+            winCanvas.gameObject.SetActive(true);
+
+
+
+        }
+    }
+
+}
