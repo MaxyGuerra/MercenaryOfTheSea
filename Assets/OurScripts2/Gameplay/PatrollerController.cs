890128f17c1d251e2a7abb4c0b48963cde8f9ff3
Autumn <66489920+autumnsbeast@users.noreply.github.com>
2020-12-28 23:55:23 -0300

    Ya hice el patrol, pero no se reinicia correctamente.
    Lo reviso mañana con calma.

---
+++ Assets/OurScripts2/Gameplay/PatrollerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrollerController : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float speed;
+
+
+    private int waypointIndex;
+    private float dist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypointIndex = 0;
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+
+    void Patrol()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void IncreaseIndex()
+    {
+        waypointIndex++;
+
+        if (waypointIndex >= waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
+
+        if(dist < 0f)
+        {
+            IncreaseIndex();
+        }
+
+        Patrol();
+    }
+
+
+
+    
+}
