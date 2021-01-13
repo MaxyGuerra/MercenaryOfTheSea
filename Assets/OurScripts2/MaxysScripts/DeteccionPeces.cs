7f5f2c825b0f6d3c736e8f23ff9f70ea85781fcf
MaxyGuerra <66489175+MaxyGuerra@users.noreply.github.com>
2021-01-13 18:11:02 -0300

    agregue a los peces pequeños en el lvl 1 y lvl 2

---
+++ Assets/OurScripts2/MaxysScripts/DeteccionPeces.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeteccionPeces : MonoBehaviour
+{
+    public GameObject peces;
+    private bool PrimeraActi;
+
+    private void Start()
+    {
+        peces.SetActive(false);
+        PrimeraActi = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && PrimeraActi == false)
+        {
+            peces.SetActive(true);
+            PrimeraActi = true;
+        }
+    }
+}
